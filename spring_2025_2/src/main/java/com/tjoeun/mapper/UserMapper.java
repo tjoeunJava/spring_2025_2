5efc3a6d16e4fc780dead72d194853d9c8ec63fe
tjoeunJava <cruiseclleague38@gmail.com>
2025-04-01 12:21:47 +0900

    UserMapper # 을 $ 로 혼동하지 말 것

--- spring_2025_2/src/main/java/com/tjoeun/mapper/UserMapper.java
+++ spring_2025_2/src/main/java/com/tjoeun/mapper/UserMapper.java
@@ -24,7 +24,7 @@ public interface UserMapper {
 	//     서버 실행할 때 자동으로 생성해 놓은 UserDTO 에 
 	//     로그인한 회원의 정보를 위함 
 	// WHERE user_id = #{user_id} AND user_pw = #{user_pw} 소스 확인할 것
-	// 주의해서 작성하기
+	// 주의해서 작성하기 # 을 $ 로 혼동하지 말 것
 	@Select("SELECT * " +
 	        "FROM user_table " +
 	        "WHERE user_id = #{user_id} AND user_pw = #{user_pw}")
